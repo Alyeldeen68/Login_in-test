b73a06eca9ad747d5dc5c9ccf7636edd48b43aa7
Alyeldeen68 <alyeldeenkhaled2@gmail.com>
2022-10-11 23:03:58 +0200

    Hamoo

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
-
+import logo from "./logo.svg";
+import "./App.css";
+import { Routes, Route } from "react-router-dom";
+import Dashboard from "./Components/Dashboard";
+import Prefences from "./Components/Preferences";
+import Login from "./Components/Login";
+import { useState } from "react";
 function App() {
+  const [token, setToken] = useState();
+  // if (!token) {
+  //   return <Login setToken={setToken} />;
+  // }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="main">
+      <h2>Main App</h2>
+      <Login />
+      <Routes>
+        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/preferences" element={<Prefences />} />
+      </Routes>
     </div>
   );
 }
---
+++ src/Components/Login.js
@@ -0,0 +1,45 @@
+import React, { useState } from "react";
+import "./Login.css";
+const Login = () => {
+  const [userName, setUserName] = useState("");
+  const [password, setPassword] = useState("");
+  return (
+    <div className="main">
+      <form className="Auth-form">
+        <div className="Auth-form-content">
+          <h3 className="Auth-form-title">Sign In</h3>
+          <div className="form-group mt-3">
+            <label>Email address</label>
+            <input
+              type="email"
+              className="form-control mt-1"
+              placeholder="Enter email"
+              onChange={(e) => {
+                setUserName(e.target.value);
+              }}
+            />
+          </div>
+          <div className="form-group mt-3">
+            <label>Password</label>
+            <input
+              type="password"
+              className="form-control mt-1"
+              placeholder="Enter password"
+              onChange={(e) => setPassword(e.target.value)}
+            />
+          </div>
+          <div className="d-grid gap-2 mt-3">
+            <button type="submit" className="btn btn-primary">
+              Submit
+            </button>
+          </div>
+          <p className="forgot-password text-right mt-2">
+            Forgot <a href="#">password?</a>
+          </p>
+        </div>
+      </form>
+    </div>
+  );
+};
+
+export default Login;
